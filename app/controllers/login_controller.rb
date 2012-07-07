dcdde42bdefcba4ede7ad6f115263c9a5583964e
Vlad <vladst@post.jce.ac.il>
2012-07-07 05:28:33 -0700

    css + students form fixed

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,22 +2,20 @@ class LoginController < ApplicationController
   def index
   end
   
-  def choose_classes
-    #@classes = Gclasses.group(:class_id).select(:class_id).map {|elem| elem.class_id}
-    teacherID = 12345 #params[:teacherID]
-    @possible_classes = Teacher.group(:class_id).select(:class_id).where(:teacher_id => teacherID).map {|elem| elem.class_id}
-    @possible_subjects = Teacher.group(:subject).select(:subject).where(:teacher_id => teacherID).map {|elem| elem.subject}
-  end
-  
   def auth
     pass=params[:Password]
     id=params[:user]
-    passFromDB=Teacher.select(:password).where(:teacher_id => id)
-    if !(passFromDB.nil? || passFromDB.empty?) && passFromDB.first.password == pass
+    teacherpassFromDB=Teacher.select(:password).where(:teacher_id => id)
+    if !(teacherpassFromDB.nil? || teacherpassFromDB.empty?) && teacherpassFromDB.first.password == pass
       redirect_to "/teachers/#{id}/choose_classes"
-    else
-      flash[:notice] = "Pair of login and password not found"
-      redirect_to "/"
+    else 
+      managerpassFromDB=Manager.select(:password).where(:manager_id => id)
+      if !(managerpassFromDB.nil? || managerpassFromDB.empty?) && managerpassFromDB.first.password == pass
+        redirect_to "/managers/#{id}/choose_classes"
+      else
+        flash[:notice] = "Pair of login and password not found"
+        redirect_to "/"
+      end
     end
   end
 end
